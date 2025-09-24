5fe9f3410448812a26f63960e55d0067bacd26da
Saku Nurmiranta <91052471+SakuNurmiranta@users.noreply.github.com>
2025-09-24 17:51:14 +0300

    More schematics

--- TheProspector/Assets/Scripts/HelloWorld.cs
+++ TheProspector/Assets/Scripts/HelloWorld.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    private int test = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hello World x" + test++);
     }
 }
