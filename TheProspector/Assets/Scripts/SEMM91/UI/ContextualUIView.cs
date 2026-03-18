ab2ed5fb63e637f7d7c8606ad56944c08f1251f9
Saku Nurmiranta <91052471+SakuNurmiranta@users.noreply.github.com>
2026-03-18 12:18:24 +0200

    Create ContextualUIView.cs

---
+++ TheProspector/Assets/Scripts/SEMM91/UI/ContextualUIView.cs
@@ -0,0 +1,12 @@
+﻿namespace SEMM91.UI
+{
+    public abstract class ContextualUIView : UIView
+    {
+        public override void Show()
+        {
+            base.Show();
+            DeActivate();
+        }
+        
+    }
+}
