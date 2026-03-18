07b68e17c5819cc45c44df2fca1529d37b023df4
Saku Nurmiranta <91052471+SakuNurmiranta@users.noreply.github.com>
2026-03-18 12:18:31 +0200

    Create PersistentUIView.cs

---
+++ TheProspector/Assets/Scripts/SEMM91/UI/PersistentUIView.cs
@@ -0,0 +1,15 @@
+﻿namespace SEMM91.UI
+{
+    public abstract class PersistentUIView : UIView
+    {
+        public override void Activate()
+        {
+            base.Activate();
+        }
+
+        public override bool Supports(GameUIState state)
+        {
+            return true;
+        }
+    }
+}
