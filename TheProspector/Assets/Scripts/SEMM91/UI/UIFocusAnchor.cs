6fcb744a417d6c93c86b68a1576d5c83f30c36f8
Saku Nurmiranta <91052471+SakuNurmiranta@users.noreply.github.com>
2026-03-18 12:18:41 +0200

    Create UIFocusAnchor.cs

---
+++ TheProspector/Assets/Scripts/SEMM91/UI/UIFocusAnchor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SEMM91.UI
+{
+    public class UIFocusAnchor : MonoBehaviour
+    {
+        [SerializeField] private GameUIState state;
+        [SerializeField] private float targetZoom = 5f;
+
+        public GameUIState State => state;
+        public float TargetZoom => targetZoom;
+        public Vector3 Position => transform.position;
+    }
+}
